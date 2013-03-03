1e9b40f10aa82b7bef7ff220d8a53f52c7e05fe4
E.Azer Koçulu <azer@kodfabrik.com>
2013-03-02 17:11:41 -0800

    hello kitties

---
+++ README.md
@@ -0,0 +1,9 @@
+## prop
+
+### Install
+
+```bash
+$ npm install prop
+```
+
+![](https://dl.dropbox.com/s/9q2p5mrqnajys22/npmel.jpg?token_hash=AAHqttN9DiGl63ma8KRw-G0cdalaiMzrvrOPGnOfDslDjw)
---
+++ test.js
@@ -0,0 +1,95 @@
+var prop    = require("./"),
+    expect = require('chai').expect;
+
+it('creates a new prop', function(){
+
+  var foo = prop();
+
+  expect(foo()).to.not.exist;
+
+  foo(3.14);
+  expect(foo()).to.equal(3.14);
+
+  foo(156);
+  expect(foo()).to.equal(156);
+
+});
+
+it('creates a new prop with default value', function(){
+
+  var foo = prop(3.14);
+
+  expect(foo()).to.equal(3.14);
+});
+
+it('creates a new prop with getter', function(){
+
+  var foo = prop(3.14, function(raw){
+    return raw + '%';
+  });
+
+  expect(foo()).to.equal('3.14%');
+  expect(foo.raw()).to.equal(3.14);
+
+});
+
+it('creates a new prop with setter', function(){
+
+  var foo = prop(3.14, null, function(raw){
+    return raw + '$';
+  });
+
+  expect(foo()).to.equal('3.14$');
+  expect(foo.raw()).to.equal('3.14$');
+
+  foo(156);
+
+  expect(foo()).to.equal('156$');
+  expect(foo.raw()).to.equal('156$');
+
+  foo.raw(156);
+
+  expect(foo()).to.equal(156);
+  expect(foo.raw()).to.equal(156);
+
+});
+
+it('has chaining methods for setting getters and setters', function(){
+
+  var foo = prop()
+        .getter(function(val){
+          return val + '.00$';
+        })
+        .setter(function(val){
+          return parseInt(val);
+        });
+
+  foo('256$');
+
+  expect(foo()).to.equal('256.00$'
+);
+
+});
+
+it('can be extended', function(){
+
+  var foo = prop(3.14)
+        .extend(function(raw){
+
+          return function newRaw(newValue){
+            if(newValue){
+              newValue*=10;
+              return raw(newValue);
+            }
+
+            return raw();
+          };
+        });
+
+
+  foo(156);
+
+  expect(foo()).to.equal(1560);
+
+
+});
