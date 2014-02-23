1e89038c1bed7506695a816b1473fa80d0f9cbd8
E.Azer Koçulu <azer@kodfabrik.com>
2014-02-23 11:52:29 -0800

    get rid of unnecessary stuff

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ## property [![Build Status](https://travis-ci.org/azer/property.png)](https://travis-ci.org/azer/property)
 
-Minimalistic, extendible property library for JavaScript 
+Library for creating functional properties
 
 ```js
+prop = require('property')
+
 PI = prop(3.14)
 PI()
 // => 3.14
@@ -15,66 +17,5 @@ PI()
 ## Install
 
 ```bash
-$ npm install new-prop # component install adaio/prop
+$ npm install property
 ```
-
-## API
-
-### `extend`
-
-```js
-foo = prop(3.14)
-  .extend(function(raw){
-
-    return function(newValue){
-      
-      if(newValue){
-        newValue*=10
-        return raw(newValue)
-      }
-
-      return raw()
-    }
-
-  })
-```
-
-### `getter(Function)`
-
-Sets a getter function for the property.
-
-```js
-foo = prop()
-  .getter(function(value){
-    return value + '.00$'
-  })
-
-foo(314)
-
-foo()
-// => 314.00$
-
-foo.raw()
-// => 314
-```
-
-### `setter(Function)`
-
-Sets a setter function for the property.
-
-```js
-foo = prop()
-  .setter(function(value){
-    return value + '.00$'
-  })
-
-foo(314)
-
-foo()
-// => 314.00$
-
-foo.raw()
-// => 314.00$
-```
-
-![](https://dl.dropbox.com/s/9q2p5mrqnajys22/npmel.jpg)
--- index.js
+++ index.js
@@ -1,54 +1,15 @@
-module.exports = prop;
+module.exports = create;
 
-/**
- * Create and return a new property.
- *
- * @param {Anything} rawValue (optional)
- * @param {Function} getter (optional)
- * @param {Function} setter (optional)
- * @return {AdaProperty}
- */
-function prop(rawValue, getter, setter){
+function create (defaultValue) {
+  var value = defaultValue;
 
-  var raw = (function(value){
+  return access;
 
-    return function raw(update){
-      if( arguments.length ){
-        value = update;
-      }
-
-      return value;
-    };
-
-  }());
-
-  function proxy(update, options){
-    if(arguments.length > 0){
-      raw( setter ? setter(update, raw()) : update );
+  function access (newValue) {
+    if (arguments.length > 0) {
+      value = newValue;
     }
 
-    return getter ? getter(raw()) : raw();
-  };
-
-  proxy.extend = function(ext){
-    raw = ext(raw);
-    return proxy;
+    return value;
   }
-
-  proxy.getter = function(newGetter){
-    getter = newGetter;
-    return proxy;
-  };
-
-  proxy.setter = function(newSetter){
-    setter = newSetter;
-    return proxy;
-  };
-
-  proxy.isAdaProperty = true;
-  proxy.raw           = raw;
-
-  raw(setter ? setter(rawValue) : rawValue);
-
-  return proxy;
 }
